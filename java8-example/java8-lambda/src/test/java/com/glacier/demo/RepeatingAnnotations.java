81b7da33be5e00165b52125dfc26a4cd237a8d69
glacier <glacier0315@126.com>
2020-04-04 19:58:30 +0800

    添加java8

---
+++ java8-example/java8-lambda/src/test/java/com/glacier/demo/RepeatingAnnotations.java
@@ -0,0 +1,41 @@
+package com.glacier.demo;
+
+import java.lang.annotation.*;
+
+/**
+ * @author diandian.zhang
+ * @ClassName:RepeatingAnnotations
+ * @Description:重复注解@Repeatable
+ * @date 2017年3月31日下午3:48:13
+ */
+public class RepeatingAnnotations {
+    @Target(ElementType.TYPE)
+    @Retention(RetentionPolicy.RUNTIME)
+    public @interface Filters {
+        Filter[] value();
+    }
+
+    @Target(ElementType.TYPE)
+    @Retention(RetentionPolicy.RUNTIME)
+    @Repeatable(Filters.class)
+    public @interface Filter {
+        String value();
+
+        String value2();
+    }
+
+    ;
+
+    @Filter(value = "filter1", value2 = "111")
+    @Filter(value = "filter2", value2 = "222")
+    //@Filters({@Filter(  value="filter1",value2="111" ),@Filter(  value="filter2", value2="222")}).注意：JDK8之前：1.没有@Repeatable2.采用本行“注解容器”写法
+    public interface Filterable {
+    }
+
+    public static void main(String[] args) {
+        //获取注解后遍历打印值
+        for (Filter filter : Filterable.class.getAnnotationsByType(Filter.class)) {
+            System.out.println(filter.value() + filter.value2());
+        }
+    }
+}
